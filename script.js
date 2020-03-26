2854b7eecd8ae46beea870884d298d096ee667d6
Topesqueen <62329505+Topesqeen@users.noreply.github.com>
2020-03-26 19:52:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let x = "Ayinla Temitope";
+console.log(x);
+
+let course = ['Html', 'Css', 'Javascript'];
+let even = course['Html', 'Css', 'Javascript'];
+console.log(course);
+for(let i =0; i < course.length; i++){
+  if (course[i] !==course[1]) {
+    even = course[i];
+    
+  }
+  console.log(even);
+  //return even;
+}
